e45710b3e3328b838c317ba55764363bcb4ae78a
Xavier Cuases <xaviercuases@hotmail.com>
2026-01-31 16:45:02 -0500

    Proyecto Spring Boot inicial con H2 configurado

---
+++ src/main/java/com/bancoapi/BancoApiApplication.java
@@ -0,0 +1,15 @@
+package com.bancoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoApiApplication.class, args);
+
+	}
+
+
+}
