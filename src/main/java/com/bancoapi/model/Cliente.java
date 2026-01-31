fa205cc4d0175d1786926c803306f1f012e45f1e
Xavier Cuases <xaviercuases@hotmail.com>
2026-01-31 17:57:10 -0500

    feat: fix herencia cliente

--- src/main/java/com/bancoapi/model/Cliente.java
+++ src/main/java/com/bancoapi/model/Cliente.java
@@ -5,22 +5,11 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "clientes")
 public class Cliente extends Persona{
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long clienteId;
 
     private String contrasena;
     private Boolean estado;
 
     // getters y setters
-
-    public Long getClienteId() {
-        return clienteId;
-    }
-
-    public void setClienteId(Long clienteId) {
-        this.clienteId = clienteId;
-    }
 
     public String getContrasena() {
         return contrasena;
