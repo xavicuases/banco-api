327c7f428e89e5ca3ef1bc9e4d06eb5564f5b64a
Xavier Cuases <xaviercuases@hotmail.com>
2026-01-31 19:34:00 -0500

    feat: Cambio para que se interfaz

--- src/main/java/com/bancoapi/repository/CuentaRepository.java
+++ src/main/java/com/bancoapi/repository/CuentaRepository.java
@@ -3,6 +3,7 @@ import com.bancoapi.model.Cuenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CuentaRepository extends JpaRepository <Cuenta, Long> {
     Cuenta findByNumeroCuenta(String numeroCuenta);
 }
