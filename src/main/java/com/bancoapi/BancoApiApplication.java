dd4cbf6d42e0912aa4b8d9225847bf0edeffaee2
Xavier Cuases <xaviercuases@hotmail.com>
2026-02-01 14:06:00 -0500

    feat: se agrega un mensaje en main para pruebas

--- src/main/java/com/bancoapi/BancoApiApplication.java
+++ src/main/java/com/bancoapi/BancoApiApplication.java
@@ -8,7 +8,7 @@ public class BancoApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BancoApiApplication.class, args);
-                System.out.println("");
+                System.out.println("PRUEBA");
 
 	}
 
