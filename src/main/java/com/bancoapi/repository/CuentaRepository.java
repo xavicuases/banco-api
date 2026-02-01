d3d0746e1b2c338e2e98297e85f34f1efa49379c
Xavier Cuases <xaviercuases@hotmail.com>
2026-01-31 19:09:44 -0500

    agregue CuentaRepository para CRUD de cuentas

---
+++ src/main/java/com/bancoapi/repository/CuentaRepository.java
@@ -0,0 +1,8 @@
+package com.bancoapi.repository;
+import com.bancoapi.model.Cuenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface CuentaRepository extends JpaRepository <Cuenta, Long> {
+    Cuenta findByNumeroCuenta(String numeroCuenta);
+}
