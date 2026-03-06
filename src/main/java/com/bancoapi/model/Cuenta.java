8eaf086ae7d9cb655c31e34d96250e439a39d1ae
Xavier Cuases <xaviercuases@hotmail.com>
2026-03-05 19:32:47 -0500

    fix: corregir recursión infinita en entidades JPA al serializar JSON

--- src/main/java/com/bancoapi/model/Cuenta.java
+++ src/main/java/com/bancoapi/model/Cuenta.java
@@ -1,5 +1,6 @@
 package com.bancoapi.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class Cuenta {
 
     // Relación con movimientos
     @OneToMany(mappedBy = "cuenta", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
     private List<Movimiento> movimientos;
 
     // Relación con clientes
