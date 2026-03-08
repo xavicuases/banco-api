2b906dbb3e20e721409365f0fa11e791644dc579
Xavier Cuases <xaviercuases@hotmail.com>
2026-03-07 20:34:51 -0500

    feat: implementar metodo en interface para listar cuentas

--- src/main/java/com/bancoapi/repository/CuentaRepository.java
+++ src/main/java/com/bancoapi/repository/CuentaRepository.java
@@ -2,9 +2,12 @@ package com.bancoapi.repository;
 import com.bancoapi.model.Cuenta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CuentaRepository extends JpaRepository <Cuenta, Long> {
     Optional<Cuenta> findByNumeroCuenta(String numeroCuenta);
+    List<Cuenta> findByClienteId(Long clienteId);
 }
