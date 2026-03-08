61eb01c81794bdaabe2899b0d7be5cf867bd4cc1
Xavier Cuases <xaviercuases@hotmail.com>
2026-03-07 20:32:40 -0500

    feat: agregar método findByNumeroCuentaAndFechaBetween en MovimientoRepository

--- src/main/java/com/bancoapi/repository/MovimientoRepository.java
+++ src/main/java/com/bancoapi/repository/MovimientoRepository.java
@@ -2,9 +2,17 @@ package com.bancoapi.repository;
 import com.bancoapi.model.Movimiento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface MovimientoRepository extends JpaRepository <Movimiento, Long> {
     List<Movimiento> findByCuentaId(Long cuentaId);
+    List<Movimiento> findByCuentaNumeroCuentaAndFechaBetween(
+            String numeroCuenta,
+            LocalDateTime fechaInicio,
+            LocalDateTime fechaFin
+    );
+
 }
